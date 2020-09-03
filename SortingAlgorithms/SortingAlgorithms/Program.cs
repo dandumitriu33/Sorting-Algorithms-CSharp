f863d46d864f5b7f3b5161b3a3c07868b997842c
Dan Dumitriu <dandumitriu33@gmail.com>
2020-09-03 20:27:46 +0300

    Add Quick Sort

--- SortingAlgorithms/SortingAlgorithms/Program.cs
+++ SortingAlgorithms/SortingAlgorithms/Program.cs
@@ -78,18 +78,49 @@ namespace SortingAlgorithms
             //Time Complexity: O(2N)
             //Auxiliary Space: O(1)
 
+            //var watch2 = System.Diagnostics.Stopwatch.StartNew();
+
+            //int[] arr2 = { 64, 25, 12, 22, 11, 50, 101, 34, 73, 44, 0, 12, 2222 };
+            //InsertionSort.Sort(arr2);
+            //Console.WriteLine("Sorted array");
+            //InsertionSort.PrintArray(arr2);
+
+            //watch2.Stop();
+            //var elapsedMs2 = watch2.ElapsedMilliseconds;
+            //Console.WriteLine($"InsertionSort: {elapsedMs2}");
+
+            #endregion
+
+            #region Quick Sort
+            //Like Merge Sort, QuickSort is a Divide and Conquer algorithm. It picks an element as pivot and partitions the 
+            //given array around the picked pivot.There are many different versions of quickSort that pick pivot in different ways.
+
+            //Always pick first element as pivot.
+            //Always pick last element as pivot (implemented below)
+            //Pick a random element as pivot.
+            //Pick median as pivot.
+            //The key process in quickSort is partition(). Target of partitions is, given an array and an element x of array as pivot, 
+            //put x at its correct position in sorted array and put all smaller elements(smaller than x) before x, and put all greater 
+            //elements(greater than x) after x. All this should be done in linear time.
+
+            //Time Complexity O(N^2)
+            //Space Complexity O(1)
+
+
             var watch2 = System.Diagnostics.Stopwatch.StartNew();
 
             int[] arr2 = { 64, 25, 12, 22, 11, 50, 101, 34, 73, 44, 0, 12, 2222 };
-            InsertionSort.Sort(arr2);
+            QuickSort.Sort(arr2, 0, arr2.Length-1);
             Console.WriteLine("Sorted array");
-            InsertionSort.PrintArray(arr2);
+            QuickSort.PrintArray(arr2);
 
             watch2.Stop();
             var elapsedMs2 = watch2.ElapsedMilliseconds;
-            Console.WriteLine($"InsertionSort: {elapsedMs2}");
+            Console.WriteLine($"QuickSort: {elapsedMs2}");
 
             #endregion
+
+
         }
     }
 }
---
+++ SortingAlgorithms/SortingAlgorithms/QuickSort.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortingAlgorithms
+{
+    public class QuickSort
+    {
+        public static void Sort(int[] arr, int low, int high)
+        {
+            if (low < high)
+            {
+                int partitioningIndex = partition(arr, low, high);
+
+                Sort(arr, low, partitioningIndex - 1);
+                Sort(arr, partitioningIndex + 1, high);
+            }
+        }
+
+        private static int partition(int[] arr, int low, int high) 
+        {
+            int pivot = arr[high];
+
+            int i = (low - 1);
+            for (int j = low; j < high; j++)
+            {
+                if (arr[j] < pivot)
+                {
+                    i++;
+
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+
+            int temp2 = arr[i + 1];
+            arr[i + 1] = arr[high];
+            arr[high] = temp2;
+
+            return i + 1;
+        }
+
+        public static void PrintArray(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
