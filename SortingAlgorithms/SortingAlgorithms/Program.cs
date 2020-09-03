7e7f5a62439aa8e4035640a31d0657a2f1b537fb
Dan Dumitriu <dandumitriu33@gmail.com>
2020-09-03 19:41:25 +0300

    Add Bubble Sort

--- SortingAlgorithms/SortingAlgorithms/Program.cs
+++ SortingAlgorithms/SortingAlgorithms/Program.cs
@@ -18,15 +18,26 @@ namespace SortingAlgorithms
             //subarray is picked and moved to the sorted subarray.
 
             //Time Complexity: O(N^2) as there are two nested loops.
-
             //Auxiliary Space: O(1)
             //The good thing about selection sort is it never makes more than O(N) swaps and can be useful when memory write is a costly operation.
 
 
+            //int[] arr = { 64, 25, 12, 22, 11 };
+            //SelectionSort.Sort(arr);
+            //Console.WriteLine("Sorted array");
+            //SelectionSort.PrintArray(arr);
+            #endregion
+
+            #region Bubble Sort
+            //Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in wrong order.
+            //Time Complexity: O(N^2)
+            //Auxiliary Space: O(1)
+
             int[] arr = { 64, 25, 12, 22, 11 };
-            SelectionSort.Sort(arr);
+            BubbleSort.Sort(arr);
             Console.WriteLine("Sorted array");
-            SelectionSort.PrintArray(arr);
+            BubbleSort.PrintArray(arr);
+
             #endregion
         }
     }
