5617fbb6bb5ebacf8155a6e7348660e7dc44f298
Dan Dumitriu <dandumitriu33@gmail.com>
2020-09-03 19:29:51 +0300

    Add selection sort

---
+++ SortingAlgorithms/SortingAlgorithms/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SortingAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Selection Sort
+
+            //The selection sort algorithm sorts an array by repeatedly finding the minimum element(considering ascending order) 
+            //from unsorted part and putting it at the beginning.The algorithm maintains two subarrays in a given array.
+
+            //1) The subarray which is already sorted.
+            //2) Remaining subarray which is unsorted.
+
+            //In every iteration of selection sort, the minimum element(considering ascending order) from the unsorted 
+            //subarray is picked and moved to the sorted subarray.
+
+            //Time Complexity: O(N^2) as there are two nested loops.
+
+            //Auxiliary Space: O(1)
+            //The good thing about selection sort is it never makes more than O(N) swaps and can be useful when memory write is a costly operation.
+
+
+            int[] arr = { 64, 25, 12, 22, 11 };
+            SelectionSort.Sort(arr);
+            Console.WriteLine("Sorted array");
+            SelectionSort.PrintArray(arr);
+            #endregion
+        }
+    }
+}
---
+++ SortingAlgorithms/SortingAlgorithms/SelectionSort.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortingAlgorithms
+{
+    public class SelectionSort
+    {
+        public static void Sort(int[] arr)
+        {
+            int n = arr.Length;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                int min_idx = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arr[j] < arr[min_idx])
+                    {
+                        min_idx = j;
+                    }
+                }
+
+                int temp = arr[min_idx];
+                arr[min_idx] = arr[i];
+                arr[i] = temp;
+            }
+        }
+
+        public static void PrintArray(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
